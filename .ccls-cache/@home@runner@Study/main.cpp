c4fee06bc47171307a285c8e942f9c7d5fcb8817
Toorop <>
2022-08-09 18:49:46 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@Study/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+int main() {
+  int variable1 = 1;
+  std::string variableStr = "Roma";
+  std::cout << "Hello World!\n";
+  std::cout << false << std::endl;
+  variable1 = 9;
+  variableStr = variableStr + " = " + "Loh";
+  std::cout << variableStr;
+  return 0;
+}
